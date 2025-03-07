11f329ff594bf1721d6457ed7bf80127c9438fd2
Benjamin Wright <benjamin.b.wright@gmail.com>
2025-03-06 23:08:47 -0600

    adds reloading for FastAPI server

--- backend/main.py
+++ backend/main.py
@@ -8,8 +8,8 @@ app.include_router(todo_routes.router, prefix="/todos", tags=["todos"])
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello, FastAPI!"}
+    return {"message": "Hello, FastAPI in Docker!"}
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
